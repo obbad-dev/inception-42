7b2dac320afc7612cf2842e4db09d8ff4c9bfef7
oulidobbad <oualidobbad@gmail.com>
2026-08-09 13:08:35 +0100

    mariadb container make it

---
+++ srcs/requirements/mariaDb/tools/script.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+set -e
+
+if [ -f /run/secrets/db_root_password ]; then
+    MYSQL_ROOT_PASSWORD=$(cat secrets/db_root_password)
+fi
+
+if [ -f /run/secrets/db_password ]; then
+    MYSQL_PASSWORD=$(cat secrets/db_password)
+fi
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ]; then
+    echo "[ERROR] Initialization failed: One or more required variables (MYSQL_ROOT_PASSWORD, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD) are empty or missing!" >&2
+    exit 1
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+    mariadbd --user=mysql &
+    pid="$!"
+
+    max_attempts=30
+    counter=0
+
+    while ! mysqladmin ping --silent; do
+        counter=$((counter + 1))
+        
+        if [ "$counter" -ge "$max_attempts" ]; then
+            echo "[ERROR] MariaDB failed to start within $max_attempts seconds" >&2
+            kill -9 "$pid" 2>/dev/null || true
+            exit 1
+        fi
+        sleep 1
+    done
+
+
+    mysql -u root << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+    wait "$pid"
+fi
+
+exec "$@"
